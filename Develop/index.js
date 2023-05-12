ec8ae2e317d9db1c66112f04c9e4640f57c7b12f
codyt93 <128422597+codyt93@users.noreply.github.com>
2023-05-12 11:57:59 -0700

    tpg

---
+++ Develop/index.js
@@ -0,0 +1,53 @@
+const inquirer = require("inquirer")
+function menu() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'choice',
+                choices: ['addManager', 'addIntern', 'addEngineer']
+            },
+
+        ])
+        .then((response) => {
+            if (response.choice === 'addManager') {
+                inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        message: 'Managers name?',
+                        name: 'name',
+                       
+                    },
+                    {
+                        type: 'input',
+                        message: 'what is your email?',
+                        name: 'email',
+                       
+                    },
+                    {
+                        type: 'input',
+                        message: 'what is your office number?',
+                        name: 'officenumber',
+                       
+                    },
+                    {
+                        type: 'input',
+                        message: 'what is your id?',
+                        name: 'id',
+                       
+                    },
+                ])
+
+            }
+            if (response.choice === 'addIntern') {
+
+            }
+            if (response.choice === 'addEngineer') {
+
+            }
+        }
+        )
+
+}
